b4e5a5d4ce64e9eae0024da7475c873d7cd2ab37
shubham Ram <shubhamram2k@gmail.com>
2024-08-09 21:50:10 +0530

    updated

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function myfunc(){
-  var email = document.querySelector("#email");
+function myfunc(x){
+  var email = document.querySelector("#email"+x);
   var emailId = email.value;
-  var error = document.getElementById("error");
-  
+  var error = document.getElementById("error"+x);
+
   var valid = emailId.search("@");
   var valid1 = emailId.search(".com");
 
@@ -16,8 +16,36 @@ function myfunc(){
   }
 }
 
-function arrowfn(){
-  document.getElementById("arrow").style.animation = "arrow 2s  1"; 
-  document.getElementById("arrow").style.animationTimingFunction=" cubic-bezier(0.76, 0.04, 0.59, 1.33)";
-  document.getElementById("arrow").style.animationFillMode = "forwards";
+function passwordMatching(){
+  var password = document.getElementById("password");
+  var confirmPassword = document.getElementById("confirm-password");
+  // console.log(password);
+  // console.log(confirmPassword);
+  if(password.value !== confirmPassword.value){
+    confirmPassword.classList.remove("mb-4");
+    document.getElementById("passwordMismatch").style.display="flex";
+  } else {
+    confirmPassword.classList.add("mb-4");
+    document.getElementById("passwordMismatch").style.display="none";
+  }
+
+
+}
+
+function arrowfn(n){
+  document.getElementById("arrow"+n).style.animation = "arrow 1s  1"; 
+  document.getElementById("arrow"+n).style.animationTimingFunction=" cubic-bezier(0.76, 0.04, 0.59, 1.33)";
+  document.getElementById("arrow"+n).style.animationFillMode = "forwards";
+} 
+
+function moveToLog(){
+  var welcome =document.getElementById("welcome").style;
+  welcome.animation="login 1s linear forwards";
+  // welcome.animationFillMode="forwards";
+}
+
+function moveToReg(){
+  var welcome =document.getElementById("welcome").style;
+  welcome.animation="register 1s linear forwards";
+  
 }
